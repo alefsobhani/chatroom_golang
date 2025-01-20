3200b33077bf51f046f95e6f1578dd94be3c0e57
Ehsan Sobhani <ehsansobhani13791379@gmail.com>
2025-01-20 07:01:06 -0800

    add NewChatServer function to start new chat on server

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/nats-io/nats.go"
+	"log"
 	"net"
 	"sync"
 )
@@ -10,6 +11,18 @@ type ChatServer struct {
 	users    map[string]net.Conn
 	usersMu  sync.RWMutex
 	natsConn *nats.Conn
+}
+
+func NewChatServer() *ChatServer {
+	nc, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to NATS: %v", err)
+	}
+
+	return &ChatServer{
+		users:    make(map[string]net.Conn),
+		natsConn: nc,
+	}
 }
 
 func main() {
