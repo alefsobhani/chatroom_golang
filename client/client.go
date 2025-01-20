379c14b1fde02c33f23257a4a81c215659d9a480
Ehsan Sobhani <ehsansobhani13791379@gmail.com>
2025-01-20 07:24:19 -0800

    add new chat function for client

--- client/client.go
+++ client/client.go
@@ -1,7 +1,18 @@
 package main
 
-import "net"
+import (
+	"log"
+	"net"
+)
 
 type ChatClient struct {
 	conn net.Conn
+}
+
+func NewChatClient(address string) *ChatClient {
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		log.Fatalf("Failed to connect to server: %v", err)
+	}
+	return &ChatClient{conn: conn}
 }
