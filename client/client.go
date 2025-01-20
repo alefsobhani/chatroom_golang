fe7f1228569313c3fec0050d9b46100549b79a41
Ehsan Sobhani <ehsansobhani13791379@gmail.com>
2025-01-20 07:25:46 -0800

    handle client can send message in network

--- client/client.go
+++ client/client.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net"
+	"os"
 )
 
 type ChatClient struct {
@@ -15,4 +18,21 @@ func NewChatClient(address string) *ChatClient {
 		log.Fatalf("Failed to connect to server: %v", err)
 	}
 	return &ChatClient{conn: conn}
+}
+
+func (cc *ChatClient) sendMessages() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Enter message: ")
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			log.Printf("Error reading input: %v", err)
+			continue
+		}
+		_, err = cc.conn.Write([]byte(text))
+		if err != nil {
+			log.Printf("Error sending message: %v", err)
+			return
+		}
+	}
 }
